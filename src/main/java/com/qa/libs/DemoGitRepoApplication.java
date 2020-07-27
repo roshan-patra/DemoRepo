70062ebcce5b3c1cfcb8d9ec960abda24872a678
Patra <DibyaRanjan.Patra.ctr@sabre.com>
2020-07-27 13:37:25 +0530

    my first coomit

---
+++ src/main/java/com/qa/libs/DemoGitRepoApplication.java
@@ -0,0 +1,13 @@
+package com.qa.libs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGitRepoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/qa/utils/TestBase.java
@@ -0,0 +1,8 @@
+package com.qa.utils;
+
+public class TestBase {
+	
+	public void testBase() {
+		System.out.println("Test Base");
+	}
+}
