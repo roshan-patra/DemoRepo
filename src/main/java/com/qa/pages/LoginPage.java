47682526df9ecb17f72d9346ccd411c6dfb50463
Patra <DibyaRanjan.Patra.ctr@sabre.com>
2020-07-27 13:45:42 +0530

    added view page

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -5,4 +5,7 @@ public class LoginPage {
 	public void loginPage() {
 		System.out.println("Log in Page");
 	}
+	public void profile_login_page() {
+		System.out.println("profile login page");
+	}
 }
---
+++ src/main/java/com/qa/pages/ViewPage.java
@@ -0,0 +1,8 @@
+package com.qa.pages;
+
+public class ViewPage {
+	
+	public void viewPage() {
+		System.out.println("View Page");
+	}
+}
